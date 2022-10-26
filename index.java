51364f30b6630ec49144caf04aa4d0c5039d6e13
crist138 <cristoballongares@gmail.com>
2022-10-25 21:31:13 -0500

    Update

--- index.java
+++ index.java
@@ -4,14 +4,38 @@ import java.util.*;
 public class index{
 
 public static void main(String[] args){
+Scanner in = new Scanner(System.in);
 
 	Cajero cajero = new Cajero();
 
-	int price =124997;
+	String tarjeta;
+	int i;
+	boolean flag = false;
 
-	System.out.println(cajero.verifyCard("111122233334444"));
+	System.out.println("BIENVENIDO AL BANCO PRIVADO NOSE");
 
-	System.out.println(price+(price*.15));
+while(!flag){
+	System.out.println("INGRESA TU TARJETA");
+	tarjeta = in.next(); // guardanos la "tarjeta"
+
+
+	int ArrLength = cajero.cards.length;
+	
+	tarjeta = tarjeta.replaceAll("[a-zA-Z]", ""); // Eliminamos las letras, si es que puso
+	tarjeta = tarjeta.replaceAll("[\\W]|_", ""); // al igul los simbolos si es que puso
+
+
+	for(i=0;i<ArrLength;i++){ // buscamos en cada espacio del array un valor que coincida con el ingreasdo
+		if(tarjeta.equals(cajero.cards[i])){
+			System.out.println("Bienvenido!");
+			flag = true;
+			
+		}
+	} 
+	if(!flag)
+	System.out.println("TU TARJETA NO COINCIDE CON NINGUNA REGISTRADA EN EL BANCO, INTENTALO DE NUEVO\n");
+}
+
 
 
 }
--- objetos/Cajero.java
+++ objetos/Cajero.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Cajero{
 	//Atributos
 	public String empresa;
-	public String[] cards = {"1111222233334444","2222333344445555"};
+	public String[] cards = {"1111222233334444","2222333344445555", "3333444455556666"};
 
 	//Metodos
 	public boolean verifyCard(String Card){
